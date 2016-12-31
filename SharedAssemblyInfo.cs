db6ccb8b36ada4c0a2da48a463a000e51a666b3b
ildoc <filippo.giomi@gmail.com>
2016-12-31 14:35:34 +0100

    v0.0.0.1

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+
+// 31/12/2016 v0.0.0.1 
