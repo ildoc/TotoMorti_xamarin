dcb43701764b48327292d8cd7a068e19dea4bb8b
il_doc <filippo.giomi@gmail.com>
2017-01-03 15:17:04 +0100

    versione 0.0.1.1

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.0.1.1")]
+[assembly: AssemblyFileVersion("0.0.1.1")]
 
 // 31/12/2016 v0.0.0.1 
+// 03/12/2017 v0.0.1.1
