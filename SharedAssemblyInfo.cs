1bdf87c3cdc18c0a8b6784e86b60e463feb7c1ed
il_doc <filippo.giomi@gmail.com>
2017-01-04 17:09:58 +0100

    fix salvataggio categorie

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,8 +1,9 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.1.2")]
-[assembly: AssemblyFileVersion("0.0.1.2")]
+[assembly: AssemblyVersion("0.0.1.3")]
+[assembly: AssemblyFileVersion("0.0.1.3")]
 
 // 31/12/2016 v0.0.0.1 
 // 03/12/2017 v0.0.1.1
 // 04/12/2017 v0.0.1.2
+// 04/12/2017 v0.0.1.3
